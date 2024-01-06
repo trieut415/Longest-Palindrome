6bf7d066f35140c2efbf6d14511c4b6c88983585
Trieu Tran <114438848+trieut415@users.noreply.github.com>
2024-01-06 14:18:01 -0800

    Update longestPalindrome.cpp

--- longestPalindrome.cpp
+++ longestPalindrome.cpp
@@ -6,61 +6,6 @@
 #include <unordered_set> 
 #include <unordered_map> 
 
-int longestPalindrome() {                                           //iterate through the dictionary and exclude dictionary words ending with N, Tra(n)
-    string dictionary_file_name = "dictionary.txt";
-    unordered_set<string> dictionary;
-    fstream file;
-    file.open(dictionary_file_name, ios::in);
-
-    if (file.is_open()) {
-        string inWord;
-        char lastLetter = 'n';
-
-        while (file >> inWord) {                        //inserting into unordered set, gives a good average-case runtime for insert
-            if (inWord.back() != lastLetter) {
-                dictionary.insert(inWord); 
-            }
-        }
-    }
-
-    file.close();
-
-    Trie trie_tree;
-    for (const string& inWord : dictionary) {
-        trie_tree.insert(inWord);                       //insert word into a trie
-    }
-
-    fstream input_file; 
-    string read_file_name = "BigData.txt";
-    input_file.open(read_file_name, ios::in);
-
-    int palindrome = 0;                                 //detect palindrome, iterate through trie to string match
-    string line;
-    while (getline(input_file, line)) {
-        for (int i = 0; i < line.length(); i++) {
-            shared_ptr<TrieNode> node = trie_tree.getRoot();
-            for (int j = i; j < line.length(); j++) {
-                char currentChar = line[j];
-                if (node->children.find(currentChar) == node->children.end()) {
-                    break; 
-                }
-                node = node->children[currentChar];
-                if (node->fullWord) {
-                    palindrome++; 
-                }
-            }
-        }
-    }
-    return palindrome;
-}
-
-
-
-
-
-
-//*******************//
-
 int SplitLeftRight(const string& inStr, int leftHalf, int rightHalf) {
     int length = 0;
     while (inStr[leftHalf] == inStr[rightHalf] && leftHalf >= 0 && rightHalf < inStr.length()) { //check if the left and right are equal in the indices of text
@@ -69,4 +14,28 @@ int SplitLeftRight(const string& inStr, int leftHalf, int rightHalf) {
         leftHalf--;
     }
     return length;
+}
+
+int longestPalindrome() {
+
+    ifstream file("BigData.txt");  //reading and adding the file to one long string
+    string text;
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            text += line;
+        }
+        file.close();
+    }
+
+    int longestPal = 0;
+    for (int i = 0; i < text.length() - 1; i++) {
+        int evenLength = SplitLeftRight(text, i, i + 1); //recursively call functino to repeatedly split the split/expand the string to go through its element
+        int oddLength = SplitLeftRight(text, i, i);
+
+    if (oddLength > longestPal) { longestPal = oddLength; } //set equal to respective value in the case that it is odd/even
+    if (evenLength > longestPal) { longestPal = evenLength; }
+
+    }
+    return longestPal;
 }
