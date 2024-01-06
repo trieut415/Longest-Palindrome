97267ed650e67deca6353f498b18c28786372fa2
Trieu Tran <114438848+trieut415@users.noreply.github.com>
2024-01-06 14:15:44 -0800

    Create longestPalindrome.h

---
+++ longestPalindrome.h
@@ -0,0 +1,18 @@
+#ifndef longestPalindrome_h
+#define longestPalindrome_h
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <cctype>
+
+using namespace std;
+
+int fiveA();
+int fiveB();
+int fiveC();
+
+#endif /* longestPalindrome_h */
